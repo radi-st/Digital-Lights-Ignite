1e873858cbe8625b017702067d6bab60adaaa679
Atanas Semerdzhiev <semerdjiev@live.com>
2022-09-09 16:58:54 +0300

    Code review notes

--- Configuration/src/test/java/org/example/ParameterizedTestFileConfigurationReader.java
+++ Configuration/src/test/java/org/example/ParameterizedTestFileConfigurationReader.java
@@ -7,43 +7,23 @@ import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ParameterizedTestFileConfigurationReader {
+public class ConfigurationTestUtilities {
 
-    public static void createConfigurationWithBothArguments(String file1, String file2, String file3, FileConfigurationReader test_reader) throws ConfigurationException, IOException {
+    public static void assertConfigurationIsReadSuccessfully(String fileContents,
+                                                             FileConfigurationReader test_reader,
+                                                             String expectedPath,
+                                                             String expectedLevel) throws ConfigurationException, IOException {
 
-        ByteArrayInputStream stream1 = new ByteArrayInputStream(file1.getBytes());
-        ByteArrayInputStream stream2 = new ByteArrayInputStream(file2.getBytes());
-        ByteArrayInputStream stream3 = new ByteArrayInputStream(file3.getBytes());
+        ByteArrayInputStream stream = new ByteArrayInputStream(fileContents.getBytes());
+        Configuration config = test_reader.createConfiguration(stream);
 
-        Configuration config1=test_reader.createConfiguration(stream1);
-        Configuration config2=test_reader.createConfiguration(stream2);
-        Configuration config3=test_reader.createConfiguration(stream3);
-
-        assertEquals("C:\\Temp",config1.getLogFilePath());
-        assertEquals("WARN",config1.getLogLevel());
-
-        assertEquals("C:\\Desktop\\logs",config2.getLogFilePath());
-        assertEquals("4",config2.getLogLevel());
-
-        assertEquals("D:\\logs",config3.getLogFilePath());
-        assertEquals("5",config3.getLogLevel());
-
+        assertEquals(expectedPath, config.getLogFilePath());
+        assertEquals(expectedLevel, config.getLogLevel());
     }
 
-    public static void createConfigurationWithOneOrMoreArgumentsMissing(String file1, String file2, String file3,String file4,
-                                                                        FileConfigurationReader test_reader) {
-
-        ByteArrayInputStream stream1 = new ByteArrayInputStream(file1.getBytes());
-        ByteArrayInputStream stream2 = new ByteArrayInputStream(file2.getBytes());
-        ByteArrayInputStream stream3 = new ByteArrayInputStream(file3.getBytes());
-        ByteArrayInputStream stream4 = new ByteArrayInputStream(file4.getBytes());
-
-        assertThrows(NoSuchElementException.class, ()->test_reader.createConfiguration(stream1));
-        assertThrows(NoSuchElementException.class, ()->test_reader.createConfiguration(stream2));
-        assertThrows(NoSuchElementException.class, ()->test_reader.createConfiguration(stream3));
-        assertThrows(NoSuchElementException.class, ()->test_reader.createConfiguration(stream4));
-
+    public static void assertConfigurationReadingFails(String fileContents, FileConfigurationReader test_reader)
+    {
+        ByteArrayInputStream stream = new ByteArrayInputStream(fileContents.getBytes());
+        assertThrows(NoSuchElementException.class, ()->test_reader.createConfiguration(stream));
     }
-    
-    
 }
